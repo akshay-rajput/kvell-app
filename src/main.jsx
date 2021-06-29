16770b189ebaab0019f83c2d138031dd980d57c8
Akshay <akshayrajput102@gmail.com>
2021-06-29 12:08:50 +0530

    chore - provide redux store to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		
+		<BrowserRouter>
+			<Provider store={store}>
+				{/* <PlaylistsProvider> */}
+				<App />
+				{/* </PlaylistsProvider>       */}
+			</Provider>
+		</BrowserRouter>
+
+	</React.StrictMode>,
+  	document.getElementById('root')
 )
