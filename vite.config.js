47e3d42d6910ce7e179f71026ba58aaffdfe3b5a
Akshay <akshayrajput102@gmail.com>
2021-06-29 12:07:00 +0530

    chore - vite.config - setup import aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+const path = require('path');
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  resolve: {
+        alias: [
+          {find: "@", replacement: path.resolve(__dirname, 'src')},
+          {find: "@/features", replacement: path.resolve(__dirname, 'src/features')},
+          {find: "@/hooks", replacement: path.resolve(__dirname, 'src/hooks')},
+          {find: "@/screens", replacement: path.resolve(__dirname, 'src/screens')}
+        ],
+  }
 })
