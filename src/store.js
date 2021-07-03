00fce5f8c48daf8def66d4c3589507ae56c81249
Akshay <akshayrajput102@gmail.com>
2021-07-03 22:10:28 +0530

    feat - store - add follow reducer

--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import {configureStore} from '@reduxjs/toolkit';
 import authenticationReducer from '@/features/authentication/authenticationSlice';
 // import postReducer from "@/features/posts/postSlice";
 // import notificationReducer from "@/features/posts/notificationSlice";
-// import profileReducer from "@/features/posts/profileSlice";
+import profileReducer from "@/features/profile/profileSlice";
+import followerReducer from "@/features/followers/followersSlice";
 // import feedReducer from "@/features/posts/feedSlice";
 
 export default configureStore({
@@ -10,7 +11,8 @@ export default configureStore({
         authentication: authenticationReducer,
         // posts: postReducer,
         // feeds: feedReducer,
-        // profiles: profileReducer,
+        profile: profileReducer,
+        follow: followerReducer,
         // notifications: notificationReducer,
     }
 })
