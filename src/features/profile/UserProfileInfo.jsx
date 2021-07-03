76a1c3bc17bc54a84dc2a8eff723b8110ed434c9
Akshay <akshayrajput102@gmail.com>
2021-07-03 22:07:14 +0530

    style - userProfileInfo - hide empty links

--- src/features/profile/UserProfileInfo.jsx
+++ src/features/profile/UserProfileInfo.jsx
@@ -5,38 +5,40 @@ export default function UserProfileInfo({userData}) {
     
     return (
         <div className="p-2 md:px-4 md:py-2">
-            <h3 className="text-gray-600 text-xl mb-1">{userData.username}</h3>
-            <h3 className="text-gray-600 text-md mb-1">{userData.name}</h3>
+            <h3 className="text-gray-600 text-xl mb-1">{userData.name}</h3>
+            <h3 className="text-gray-600 text-md mb-1">@{userData.username}</h3>
             <p className="text-gray-500 text-sm">
                 {userData.bio}
             </p>
-            <div className="flex my-4 flex-wrap">
+            <div className="flex my-2 gap-4 flex-wrap">
                 {
                     userData.location &&
-                    <div className="user-location flex items-center gap-x-1 text-sm text-gray-500 mr-4">
+                    <div className="user-location flex items-center gap-x-1 text-sm text-gray-500">
                         <FaMapMarkerAlt className="" />{userData.location}
                     </div>
                 }
 
-                <div className="user-joined flex items-center gap-x-1 text-sm text-gray-500 mr-4">
+                <div className="user-joined flex items-center gap-x-1 text-sm text-gray-500">
                     <FaBirthdayCake className="" /> 
                     <span>Joined :</span>
                     {new Date(userData.createdAt).toDateString().substring(4)}
                 </div>
                 
-                <div className="user-links flex items-center">
+                <div className="user-links flex gap-x-3 items-center">
                     {
                         userData.links?.length > 0 && userData.links.map(link => {
                             return(
-                                <a href={link.url} key={link.linkType} className="mr-1 text-red-400" target="_blank" rel="noopener noreferrer">
+                                link.url !== "" ? 
+                                <a href={link.url} key={link.linkType} className="text-red-400" target="_blank" rel="noopener noreferrer">
                                     {
-                                        link.linkType === "Instagram" ? <GrInstagram className="mr-2"/> :
-                                        link.linkType === "Facebook" ? <GrFacebook className="mr-2" /> :
-                                        link.linkType === "LinkedIn" ? <GrLinkedin className="mr-2" /> :
-                                        link.linkType === "Twitter" ? <GrTwitter className="mr-2" /> :
-                                        <FaExternalLinkAlt className="mr-2" />
+                                        link.linkType === "Instagram" ? <GrInstagram className=""/> :
+                                        link.linkType === "Facebook" ? <GrFacebook className="" /> :
+                                        link.linkType === "LinkedIn" ? <GrLinkedin className="" /> :
+                                        link.linkType === "Twitter" ? <GrTwitter className="" /> :
+                                        <FaExternalLinkAlt className="" />
                                     }
                                 </a>
+                                : null
                             )
                         })
                     }
