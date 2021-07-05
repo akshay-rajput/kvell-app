bde351e5b582d186c32af9772d90115f1b93cfe8
Akshay <akshayrajput102@gmail.com>
2021-07-05 19:14:56 +0530

    feat - followerSlice - add follow and unfollow thunks

--- src/features/followers/followersSlice.js
+++ src/features/followers/followersSlice.js
@@ -25,6 +25,51 @@ export const getUserFollowerData = createAsyncThunk("followers/getUserFollowerDa
     }
 })
 
+// follow a user
+export const followUser = createAsyncThunk("followers/followUser", async({userId, followUserId}) => {
+    console.log("myId: "+userId+' === follow: ', followUserId);
+    
+    try{
+        const response = await axios.request ({
+            url: getUrl("updateFollow", {followUserId}),
+            method: 'post',
+            headers: {
+                'userid': userId
+            }
+        });
+
+        console.log('response of follow: ', response);
+        return response.data;
+    }
+    catch(error){
+        console.log('error following: ', error);
+        rejectWithValue(null);
+    }
+});
+
+// unfollow a user
+export const unfollowUser = createAsyncThunk("followers/unfollowUser", async({userId, followUserId}) => {
+    // console.log("myId: "+userId+' === unfollow: ', followUserId);
+
+    try{
+        const response = await axios.request ({
+            url: getUrl("updateFollow", {followUserId}),
+            method: 'delete',
+            headers: {
+                'userid': userId
+            }
+        });
+        console.log('response of unfollow: ', response);
+        
+        return {userId, followUserId};
+    }
+    catch(error){
+        console.log('error unfollowing..', error);
+        rejectWithValue(null);
+    }
+    
+});
+
 const initialFollowerData = {
     followers: [],
     following: [],
@@ -48,6 +93,38 @@ export const followersSlice = createSlice({
             state.status = "Fulfilled";
         },
         [getUserFollowerData.rejected] : (state, action) => {
+            state.status = "Rejected"
+        },
+
+        // follow
+        [followUser.pending] : (state, action) => {
+            state.status = "Loading"
+        },
+        [followUser.fulfilled] : (state, action) => {
+            // console.log("current state - ", current(state) );
+            state.followers = action.payload.followers;
+            state.following = action.payload.following;
+            // state.followers = action.payload.followers;
+            state.status = "Fulfilled";
+        },
+        [followUser.rejected] : (state, action) => {
+            state.status = "Rejected"
+        },
+
+        // unfollow
+        [unfollowUser.pending] : (state, action) => {
+            state.status = "Loading"
+        },
+        [unfollowUser.fulfilled] : (state, action) => {
+            // console.log("current state - ", current(state) );
+            const {userId, followUserId} = action.payload;
+            let newFollowerList = state.followers.filter((follower) => follower.userId._id !== userId);
+            console.log('newFOllowers: ', newFollowerList.length);
+
+            state.followers = newFollowerList;
+            state.status = "Fulfilled";
+        },
+        [unfollowUser.rejected] : (state, action) => {
             state.status = "Rejected"
         },
 
