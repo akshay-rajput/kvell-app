1811c4e184794a003451df13902ff918eae8d962
Akshay <akshayrajput102@gmail.com>
2021-07-11 22:16:03 +0530

    feat - home - add user and general feed

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import AddPost from "@/features/posts/AddPost";
+import UserFeed from '../features/feed/UserFeed';
+import GeneralFeed from '../features/feed/GeneralFeed';
 
 export default function Home() {
     return (
         <div>
             <AddPost />
+            <UserFeed />
+            <GeneralFeed />
         </div>
     )
 }
