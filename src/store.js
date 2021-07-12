78e126ddc5714731cdb488a807f25dc4f00b59dd
Akshay <akshayrajput102@gmail.com>
2021-07-12 19:24:54 +0530

    feat - store - add post reducer slice

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authenticationReducer from '@/features/authentication/authenticationSlice';
-// import postReducer from "@/features/posts/postSlice";
+import postReducer from "@/features/posts/postSlice";
 // import notificationReducer from "@/features/posts/notificationSlice";
 import profileReducer from "@/features/profile/profileSlice";
 import followerReducer from "@/features/followers/followersSlice";
@@ -9,7 +9,7 @@ import feedReducer from "@/features/feed/feedSlice";
 export default configureStore({
     reducer: {
         authentication: authenticationReducer,
-        // posts: postReducer,
+        post: postReducer,
         feed: feedReducer,
         profile: profileReducer,
         follow: followerReducer,
