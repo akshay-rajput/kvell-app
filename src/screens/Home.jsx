3c428d42a6aeb553a22e14b466d311731ff19665
Akshay <akshayrajput102@gmail.com>
2021-07-15 18:48:05 +0530

    style - home - hide addpost on mobile

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import AddPost from "@/features/posts/AddPost";
 import UserFeed from '../features/feed/UserFeed';
 import GeneralFeed from '../features/feed/GeneralFeed';
+import useWindowDimensions from "@/hooks/useWindowDimensions";
 
 export default function Home() {
+    const {width, height} = useWindowDimensions();
+
     return (
         <div>
-            <AddPost />
+            {
+                width >= 768 ?
+                <AddPost /> : null
+            }
             <UserFeed />
             <GeneralFeed />
         </div>
