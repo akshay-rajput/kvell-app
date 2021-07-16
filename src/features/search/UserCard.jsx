ea980c637b4b561dec230c153b54218799045f6d
Akshay <akshayrajput102@gmail.com>
2021-07-16 21:55:01 +0530

    feat - userCard - show numberOfPosts if provided

--- src/features/search/UserCard.jsx
+++ src/features/search/UserCard.jsx
@@ -13,22 +13,35 @@ const UserInfoCard = styled.div`
     padding: 0.5rem;
 `;
 
-export default function UserCard({user}) {
+export default function UserCard({user, numberOfPosts}) {
     return (
         <UserInfoCard className="flex gap-x-2 shadow-sm justify-between items-center">
-            <div className="flex gap-x-2">
+            <div className="flex gap-x-2 flex-grow">
                 <Link to={"/profile/"+user._id} >
                     <Avatar avatarSize={"medium"} avatarUrl={user.avatarUrl} />
                 </Link>
                 
-                <div className="flex flex-col">
-                    <Link to={"/profile/"+user._id} >
-                        <h3>{user.name}</h3>
-                    </Link>
-                    <small className="text-gray-500">@{user.username}</small>
-                </div>
-            </div>
+                {
+                    numberOfPosts ? 
+                    <div className="flex flex-wrap flex-grow justify-between">
+                        <div className="">
+                            <Link to={"/profile/"+user._id} >
+                                <h3>{user.name}</h3>
+                            </Link>
+                            <small className="text-xs text-gray-500">@{user.username}</small>
+                        </div>
 
+                        <small className="text-xs py-1 text-gray-400">{numberOfPosts} Posts</small>
+                    </div>
+                    :
+                    <div className="flex flex-col">
+                        <Link to={"/profile/"+user._id} >
+                            <h3>{user.name}</h3>
+                        </Link>
+                        <small className="text-gray-500">@{user.username}</small>
+                    </div>
+                }
+            </div>
             {/* <div className="">
                 <Button variant="contained" className="btn-primary">
                     <MdAdd className="text-lg mr-1" /> Follow
